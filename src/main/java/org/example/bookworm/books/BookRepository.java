a01833f6a99f68554d48d023907b2c5d76df75c5
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-03-10 15:46:34 +0100

    Add `@Repository` annotation to `BookRepository` interface

--- src/main/java/org/example/bookworm/books/BookRepository.java
+++ src/main/java/org/example/bookworm/books/BookRepository.java
@@ -1,6 +1,8 @@
 package org.example.bookworm.books;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends ListCrudRepository<Book, Long> {
 }
