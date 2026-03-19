84c1a70dad017ec84eb25c1b831562f0ee09faa0
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-03-19 18:17:33 +0100

    Add initial dataset to `data.sql` and configure deferred datasource initialization

---
+++ src/main/resources/data.sql
@@ -0,0 +1,61 @@
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (1, 'J. R. R. Tolkien',
+       'Ringens brödraskap är första delen av Ringarnas herre som nyöversattes 2004-2005 av Erik Andersson och Lotta Olsson.',
+       '9789113084909', 'Swedish', 511, '2019-08-28', 'Norstedts', 'Ringens brödraskap');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (2, 'J. R. R. Tolkien',
+        'De två tornen är andra delen av Ringarnas herre, som nyöversattes 2004-2005 av Erik Andersson och Lotta Olsson.',
+        '9789113084916', 'Swedish', 414, '2019-08-28', 'Norstedts', 'De två tornen');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (3, 'J. R. R. Tolkien',
+        'Konungens återkomst är tredje delen av Ringarnas herre, som nyöversattes 2004-2005 av Erik Andersson och Lotta Olsson.',
+        '9789113084923', 'Swedish', 523, '2019-08-28', 'Norstedts', 'Konungens återkomst');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (5, 'Fjodor Dostojevskij',
+        'Brott och straff har kallats ”världens mest berömda roman” och är en av världens mest lästa böcker. Sedan den kom ut 1866 har hundratals miljoner läsare följt den fattige studenten och mördaren Raskolnikov på hans febervandring genom S:t Petersburg.',
+        '9789100808020', 'Swedish', 810, '2024-09-20', 'Albert Bonniers Förlag', 'Brott och straff');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (6, 'Fjodor Dostojevskij',
+        'Det börjar bli sommar, Petersburg töms på folk och den unge berättaren känner sig allt - mer övergiven och ensam. Så en natt, under en av sina långa promenader, ser han den vackra Nastenka stå och gråta vid räcket till kanalen. De slår följe hem, och under de kommande tre nätterna fortsätter de att träffas och dela med sig av sina historier. Nastenka berättar att det var sin fästman hon väntade på, i ett helt år har hon väntat, och ännu har han inte kommit. Den unge mannen lovar att hjälpa henne hitta honom, samtidigt som hans egna känslor för Nastenka börjar växa till något mer och starkare än han någonsin upplevt.' ||
+        'Fjodor Dostojevskijs Vita nätter gavs ut första gången 1848. Sedan 1950-talet har den stått som förlaga till ett tiotal filmer, bland andra Luchino Viscontis Vita nätter från 1957 och Robert Bressons Fyra nätter i Paris från 1971. I översättning av Ellen Rydelius.',
+        '9789181082654', 'Swedish', 63, '2025-06-09', 'Modernista', 'Vita nätter');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (7, 'Fjodor Dostojevskij',
+        'Stavrogin och Pjotr är ledare för en rysk anarkistisk terrorcell, vars mål är att störta tsaren, förstöra det rådande samhället och gripa makten. De tränar attentatsmän som är villiga att offra livet för att gruppens mål ska uppnås. Men är rekryterna beredda att döda en av sina egna för att undvika upptäckt? Lojalitet, hängivenhet, självuppoffring - när gruppen hamnar under press står allt på spel i en lika grym som komisk berättelse om mål och medel, mänskliga brister och förvridna ideal.',
+        '9789174990270', 'Swedish', 862, '2021-08-06', 'Modernista', 'Onda andar');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (8, 'Fjodor Dostojevskij',
+        'Idioten är en av Fjodor Dostojevskijs allra främsta romaner. Dess hjälte, furst Myskin, är en av den ryska litteraturens mest betagande karaktärer - en överkänslig livsfrämling i en självisk och brutal omgivning. Myskin, just hemkommen från en sjukdomsvistelse i Schweiz, kastas in i Petersburgs socitetsliv. Där förälskar han sig i Natasia och Aglaja - två av världslitteraturens mest fascinerande kvinnogestalter. Inför öppen ridå utspelas fruktansvärda tragedier, groteska farser och erotiska dramer.',
+        '9789174292312', 'Swedish', 757, '2012-01-08', 'Albert Bonniers Förlag', 'Idioten');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (9, 'Fjodor Dostojevskij',
+        'Händelserna kretsar kring mordet på Fjodor Karamazov, godsägare och far till tre eller egentligen fyra söner. Patriarken, egoisten, den hänsynslöse gamle saknade inte fiender. En av hans söner misstänks för mordet. Dmitrij är oskyldig men känner ändå skuld, eftersom han faktiskt önskat ha ihjäl sin far.',
+        '9789146151951', 'Svenska', 843, '1986-06-07', 'Wahlström & Widstrand', 'Bröderna Karamazov');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (10, 'Leo Tolstoj',
+       'Krig & Fred är Leo Tolstojs episka panorama över 1800-talets Ryssland, och över Napoleons invasion av landet som - till följd av Rysslands ödeläggande av stora landområden såväl som en sträng vinter - ledde fram till hans slutgiltiga fall från makten. Romanen ges här ut i två delar, varav denna är den första. I översättning av Hjalmar Dahl.',
+       '9789176452325', 'Swedish', 650, '2025-01-27', 'Modernista', 'Krig och fred (del 1)');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (11, 'Leo Tolstoj',
+       'Krig & Fred är Leo Tolstojs episka panorama över 1800-talets Ryssland, och över Napoleons invasion av landet som - till följd av Rysslands ödeläggande av stora landområden såväl som en sträng vinter - ledde fram till hans slutgiltiga fall från makten. Romanen ges här ut i två delar, varav denna är den andra. I översättning av Hjalmar Dahl.',
+       '9789180637671', 'Swedish', 690, '2025-01-27', 'Modernista', 'Krig och fred (del 2)');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (12, 'Mary Shelley',
+        'Victor Frankenstein är en hängiven naturvetenskapsman. Studierna i Ingolstadt - kemi, medicin, anatomi - leder till en besatthet av livets uppkomst och dödens mysterium. Han vill väcka liv i döda ting för att komma närmare skapelsens ursprung och bestämmer sig för att sätta samman en varelse av kroppsdelar från avlidna människor. Men hans försök att ta Skaparens plats blir ödesdigert, och han flyr det monster som vaknar - en ensam, vilsen och skräckinjagande figur som söker sig till människor i de omgivande byarna men jagas på flykten och slutligen bestämmer sig för att hämnas.',
+        '9789176451977', 'Swedish', 238, '2020-05-13', 'Modernista', 'Frankenstein');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (13, ' Emily Brontë',
+        'När Emily Brontë [1818 -1848] dog endast 30 år gammal efterlämnade hon vad som i dag kallas världens mest klassiska kärleksroman, Svindlande höjder. Berättelsen om hur hittebarnet Heathcliff skapar ohanterligt starka band till sin välgörares dotter, Catherine Earnshaw, De är själsfränder men socialt sett tillhör de olika världar. Den våg av frustrerad längtan och destruktiv passion som följer när Catherine avböjer att gifta sig med Heathcliff är unik i världslitteraturen. I översättning av Gunilla Nordlund.',
+        '9789174993974', 'Swedish', 388, '2014-07-29', 'Modernista', 'Svindlande höjder');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (14, 'Jane Austen',
+        'Kärleken mellan Elisabeth Bennet och Fitzwilliam Darcy är en kamp. Han har sin stolthet, hon har sin krassa blick på världen. Lika tydlig som Jane Austens borgerliga roman är i skildringen av kärleksrelationer som maktmedel, lika mångbottnade och föränderliga är karaktärerna; lika levande är miljöerna och landskapet. Inte för intet är Stolthet & fördom en av historiens mest lästa och omtalade romaner. I svensk översättning av Carl Axel Ringenson, reviderad av Gösta Olzon. ',
+        '9789178934188', 'Swedish', 345, '2020-05-29', 'Modernista', 'Stolthet och fördom');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (15, 'Jane Austen',
+        'Marianne Dashwood är impulsiv och låter sig lätt dras med i de stora känslornas spel. Motsatsen till sin förståndiga och balanserade syster Elinor. Marianne, passionerad och romantisk, faller för den charmiga men opålitliga John Willoughby, medan Elinor stoiskt hanterar nyheten om att den hon älskar, Edward Ferrars, är trolovad med en annan. Jane Austens första roman Förnuft & känsla skildrar med unik exakthet övergången mellan upplysning och romantik, genom två oförglömliga kvinnogestalter. Historien om Marianne och Elinor Dashwood har aldrig slutat fascinera läsare världen över. En av den engelska litteraturens stora klassiker, här i översättning av Marie-Louise Elliott.',
+        '9789178934195', 'Swedish', 377, '2020-05-29', 'Modernista', 'Förnuft och känsla');
+INSERT IGNORE INTO books (id, author, description, isbn, language, pages, publication_date, publisher, title)
+VALUES (16, 'J. R. R. Tolkien',
+        'Den lille hobbiten Bilbo Secker dras av trollkarlen Gandalf grå med på äventyr tillsammans med tretton dvärgar, ledda av den sturske Thorin Ekensköld. De ska röva bort en stor guldskatt som vaktas av den eldsprutande draken Smaug. På vägen stöter de på ruskiga troll och vättar, men också hjälpsamma varelser som alver, jätteörnar och den store Beorn. Bilbo träffar också den slemmige Gollum som håller till i en mörk grotta uppe i bergen. Gollum utmanar Bilbo på en tävling och blir där av med sin magiska osynlighetsring. En ring som kommer att spela en viktig roll i berättelsen.',
+        '9789113084893', 'Swedish', 276, '2019-08-28', 'Norstedts', 'Hobbiten');
